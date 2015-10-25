f2a39bbdc63969f2f275f7aee5b10a5fce18b6c7
Doug MacDonald <djbmacdonald@uchicago.edu>
2015-10-25 14:26:22 -0400

    created menu. can toggle hidden. other buttons not functional yet

--- app/scripts/common.js
+++ app/scripts/common.js
@@ -1,3 +1,15 @@
+var mode = '';
+
 function clearSelected(){
   $('*').removeClass('selected').removeClass('adj');
 };
+
+function setMenu(){
+  $('.hamburger').click(function(){
+    $('#menu').toggleClass('hidden');
+  });
+};
+
+$(document).ready(function(){
+  setMenu();
+});
